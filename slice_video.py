4c5a9a4373965b507a33689d71afd265479e2c41
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2019-07-31 11:53:05 -0400

    Updated slice_video.py
    cleaned up code, adapted for general use, added a file extension variable

--- slice_video.py
+++ slice_video.py
@@ -1,11 +1,17 @@
+#adapted from:
 #https://github.com/SeedlingsBabylab/subregsplice/blob/master/subrsplice.py#L56
 import csv
 import os
 
 
 
-csvInputFile = 'compiledValues.csv'
+csvInputFile = 'verbs_onsets.csv' #change to csv file name
+#csv file format:
+# column 1 - file name without extension, column 2 - utterance onset (not used), column 3 - utterance offset (not used), column 4 - verb, column 5 - verb onset
+
+extension = ".mp4" #change to video file extension (.mov, .mp4, etc)
 csvHolder = []
+
 with open(csvInputFile, newline='') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 	for row in reader:
@@ -14,17 +20,13 @@ csvFinal = [[0 for x in range(4)] for y in range(len(csvHolder))]
 
 counter = 1
 for row in range(0,len(csvHolder)): 
-	csvFinal[row][0] = str(csvHolder[row][0]) + '.mov' #set file input name
-	csvFinal[row][2] = str(float(csvHolder[row][1])-3)#set onset time
-	csvFinal[row][3] = str(5) #set offset time
-	template = "{}_{}_{}_{}.mov"
-	prefix = str(csvFinal[row][0]).replace('.mov','')
-	kidID = prefix[-5:]
-	#onset = "{:0.2f}".format(float(csvFinal[row][2]))
-	#onset_Dash = onset.replace(".","-")
-	verb = csvHolder[row][2]
-	#csvFinal[row][1] = template.format(verb,kidID,counter,onset_Dash) #set file output name
-	csvFinal[row][1] = template.format(verb,kidID,counter,"5sec") #set file output name
+	csvFinal[row][0] = str(csvHolder[row][0]) + extension #set file input name
+	csvFinal[row][2] = str(float(csvHolder[row][4])-3)#set onset time
+	csvFinal[row][3] = str(5) #set offset time or time difference
+	template = "{}_{}_{}_{}{}"
+	kidID = str(csvFinal[row][0]).replace(extension,'')
+	verb = csvHolder[row][3]
+	csvFinal[row][1] = template.format(verb,kidID,counter,"5sec",extension) #set file output name
 	counter= counter+1
 
 
@@ -33,12 +35,15 @@ for row in range(0,len(csvFinal)):
 	timeDiff = csvFinal[row][3] #grabs time difference
 	fileName = csvFinal[row][0] #sets input filename
 	outputPath = csvFinal[row][1] #sets output path
-	#command = ["ffmpeg","-ss",str(onsetTime),"-t",str(timeDiff),"-i",str(fileName),str(outputPath),"-y"] #normal command, leaves audio on video file
-	command = ["ffmpeg","-ss",str(onsetTime),"-t",str(timeDiff),"-i",str(fileName),"-an",str(outputPath),"-y"] #cuts videos and strips audio
+	command = ["ffmpeg","-ss",str(onsetTime),"-t",str(timeDiff),"-i",str(fileName),str(outputPath),"-y"]
+	#command = ["ffmpeg","-ss",str(onsetTime),"-t",str(timeDiff),"-i",str(fileName),"-an",str(outputPath),"-y"] #for muting
 	command_string = " ".join(command)
 	print(command_string)
 	os.system(command_string)
+
+#below for adding beep
+'''
 	beepString = str(outputPath).replace(".mov","_beep.mov")
-	command2 = ["ffmpeg","-i", str(outputPath), "-itsoffset","3","-i","beep2.mp3", "-map","0:0","-map","1:0","-preset","ultrafast",beepString] #adds beep to the video at the verb onset time
+	command2 = ["ffmpeg","-i", str(outputPath), "-itsoffset","3","-i","beep2.mp3", "-map","0:0","-map","1:0","-preset","ultrafast",beepString]
 	command_string2 = " ".join(command2)
-	os.system(command_string2)
+	os.system(command_string2)'''
